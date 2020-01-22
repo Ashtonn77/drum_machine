35aeda045c932a352c3413605b379d317f5595fd
Ashton <ashtonrbay@gmail.com>
2020-01-22 15:49:55 +0200

    added App.js

--- my-beat-maker/src/App.js
+++ my-beat-maker/src/App.js
@@ -2,77 +2,77 @@ import React from 'react';
 import './App.css';
 import data from './samples'
 
-class DrumPad extends React.Component{
+class DrumPad extends React.Component {
 
- componentDidMount(){
-   document.addEventListener('keydown', this.handleKeyDown)
-   window.focus();
- } 
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown)
+    window.focus();
+  }
 
-componentWillUnmount(){
-  document.removeEventListener('keydown', this.handleKeyDown)
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }
+
+  handleKeyDown = e => {
+    if (e.keyCode === this.props.letter.charCodeAt())
+      this.audio.play();
+    this.audio.currentTime = 0;
+    this.props.handleDisplay(this.props.id);
+  }
+
+  handleClick = () => {
+    this.audio.play();
+    this.audio.currentTime = 0;
+    this.props.handleDisplay(this.props.id);
+  }
+
+  render() {
+    return (
+      <div className="drum-pad"
+        id={this.props.id}
+        onClick={this.handleClick}
+      >
+        <h3>{this.props.letter}</h3>
+        <audio
+          ref={ref => this.audio = ref}
+          className='clip'
+          id={this.props.letter}
+          src={this.props.src}>
+        </audio>
+      </div>
+    )
+  }
 }
 
-handleKeyDown = e => {
-  if(e.keyCode === this.props.letter.charCodeAt())
-  this.audio.play();
-  this.audio.currentTime = 0;
-  this.props.handleDisplay(this.props.id);
-}
-
-handleClick = () => {
-  this.audio.play();
-  this.audio.currentTime = 0;
-  this.props.handleDisplay(this.props.id);
-}
-
-render(){
-return(
-<div className="drum-pad"
-id={this.props.id}
-onClick={this.handleClick}
->
-<h3>{this.props.letter}</h3>
-<audio 
-ref = { ref => this.audio = ref }
-className='clip'
-id={this.props.letter}
-src={this.props.src}>
-</audio>
-</div>
-)
-}
-}
-
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      display: 'Let\'s make some music, shall we?' 
+      display: 'Let\'s make some music, shall we?'
     }
   }
 
-  handleDisplay = display => this.setState({display})
+  handleDisplay = display => this.setState({ display })
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="container">
 
-      <div className="drum-machine">
-    <div className="display">{this.state.display}</div>
+        <div className="drum-machine">
+          <div className="display">{this.state.display}</div>
 
-     {data.map( (drum, index) => (
-        <DrumPad
-        key={index}
-        id={drum.id}
-        letter={drum.letter}
-        src={drum.src}
-        handleDisplay={this.handleDisplay}
-        onClick={this.handleClick}
-        />
-      ))}
+          {data.map((drum, index) => (
+            <DrumPad
+              key={index}
+              id={drum.id}
+              letter={drum.letter}
+              src={drum.src}
+              handleDisplay={this.handleDisplay}
+              onClick={this.handleClick}
+            />
+          ))}
 
-      </div>
+        </div>
 
       </div>
     )
